2abcb787d4c8f4626e676e62421c876305dcdbe1
mthgatti@gmail.com <mthgatti@gmail.com>
2020-11-29 20:27:30 -0300

    Try implementing lamport

--- src/app.js
+++ src/app.js
@@ -9,7 +9,14 @@ import readConfigFile from './utils/readConfigFile';
  * */ 
 var dgram = require('dgram');
 const os = require('os');
-const socket_server = dgram.createSocket({ type:'udp4', reuseAddr: true});
+const socket_server = dgram.createSocket({ type:'udp4', reuseAddr: true})
+var nodes = [];
+
+//relogio local
+var clock = 0;
+//contador de eventos que deve ser limitado em 100
+var eventCount = 0;
+var localId;
 /*** */
 /* ============================================================================== */
 const multicastAddress = '239.42.42.42';
@@ -55,11 +62,51 @@ function setupSocket(){
 				console.log(`Error adding socket membership: ${JSON.stringify(error)}`);
 			}
 		}
-	}
+  }
   setInterval(sendMessage, 2000);
 }
 
 /* ================================================================================ */
+
+/**
+ * Método com a ideia de fazer o envio de mensagem para todos os nodos disponiveis
+ **/
+function sendMessageToNodes() {
+
+      //calcular um número aleatorio entre 0 ate length-1
+      //de acordo com o numero enviar para esse destino
+      //valorRelogio_
+      var receivingNode = nodes[randomInteger(0,nodes.length-1)];
+      var receivingId = receivingNode[0];
+      var receivingHost = receivingNode[1];
+      var receivingPort = receivingNode[2];
+      var receivingChance = receivingNode[3];
+
+      var localOrSend = Math.random();
+      if (localOrSend <= receivingChance){
+          localOrSend = 2;
+      }else localOrSend = 1;
+
+      // Local event
+      if (localOrSend === 1) {
+        clock += clock+1;
+       
+        var message = Date.now+''+localId+''+clock+''+receivingId;
+        console.log(message);
+      }
+      else if (localOrSend === 2) {
+        //falta formatar o restante da mensagem
+        var syncMsg = new Buffer('s ' + id + ' ' + clock);
+        server.send(syncMsg, 0, syncMsg.length, receivingPort, receivingHost, function (err, bytes) {
+          if (err) throw err;
+        });
+
+        var out = 's ' + receivingId + ' ' + clock
+        history += ' ' + out;
+        console.log(out);
+      }
+      eventCount++;
+}
 
 // Para ler do terminal as solicitações de arquivo
 const readline = require('readline');
@@ -119,9 +166,10 @@ async function showOptions() {
                 id: Number.parseInt(lineData[0]),
                 host: lineData[1],
                 port: Number.parseInt(lineData[2]),
-                chance: Number.parseFloat(lineData[3])
+                chance: Number.parseFloat(lineData[3]),
+                clock: 0
               };
-
+              nodes.push(info);
               console.log(colors.yellow, `informações: - ID: ${info.id} - HOST: ${info.host} - PORT: ${info.port} - CHANCE: ${info.chance}`);
             }
           })
